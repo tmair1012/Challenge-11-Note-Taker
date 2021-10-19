c1f4888e143e7fe5a482a1d8a3571ce7e455087c
TylerMair <tyleramair@gmail.com>
2021-10-18 18:42:48 -0700

    added * route

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -11,5 +11,7 @@ app.get('/' , (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 });
-
+app.get('*' , (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
 module.exports = app;
