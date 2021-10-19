1de4f77a1b7e78ffe8af3818f2f7b537f7a32634
TylerMair <tyleramair@gmail.com>
2021-10-18 18:29:46 -0700

    created post routes and delete routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,4 +1,4 @@
-var app =require("express").Router();
+var app = require("express").Router();
 var dbNotes = require("../db/db.json")
 var fs = require('fs')
 
@@ -9,3 +9,33 @@ app.get('/api/notes', (req, res) => {
     res.json(dbNotes);
 })
 
+app.post('/api/notes', (req, res)=> {
+    var newNote = {
+        id: Math.floor(Math.random() * 100 + 1 ),
+        title: req.body.title,
+        text: req.body.text
+    }
+    dbNotes.push(newNote)
+    fs.writeFileSync('/db/db.json', JSON.stringify(dbNotes), function(err,data){
+        if(err) throw err;
+    })
+    console.log('post route', dbNotes);
+    res.json(dbNotes);
+})
+
+app.delete('/api/notes/:id', (req, res)=> {
+    var updateNote = []
+    for(let i = 0; i<dbNotes.length; i++){
+        if(dbNotes[i].id != req.params.id){
+            updateNote.push(dbNotes[i])
+        }
+    }
+    dbNotes = updatedNote
+    fs.writeFileSync('/db/db.json', JSON.stringify(dbNotes), function(err,data){
+        if(err) throw err;
+    })
+    console.log('delet route', dbNotes);
+    res.json(dbNotes);
+})
+
+module.exports = app;
