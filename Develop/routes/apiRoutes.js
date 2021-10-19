46d2fb545086200ceaad3db06e7204833352746a
TylerMair <tyleramair@gmail.com>
2021-10-18 18:38:44 -0700

    adding notes works correctly, need to fix deleting notes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,7 +4,7 @@ var fs = require('fs')
 
 //Render db.json api to save to notes
 app.get('/api/notes', (req, res) => {
-    dbNotes = JSON.parse(fs.readFileSync('/db/db.json'))
+    dbNotes = JSON.parse(fs.readFileSync('./db/db.json'))
     console.log('get route', dbNotes)
     res.json(dbNotes);
 })
@@ -16,7 +16,7 @@ app.post('/api/notes', (req, res)=> {
         text: req.body.text
     }
     dbNotes.push(newNote)
-    fs.writeFileSync('/db/db.json', JSON.stringify(dbNotes), function(err,data){
+    fs.writeFileSync('./db/db.json', JSON.stringify(dbNotes), function(err,data){
         if(err) throw err;
     })
     console.log('post route', dbNotes);
@@ -31,7 +31,7 @@ app.delete('/api/notes/:id', (req, res)=> {
         }
     }
     dbNotes = updatedNote
-    fs.writeFileSync('/db/db.json', JSON.stringify(dbNotes), function(err,data){
+    fs.writeFileSync('./db/db.json', JSON.stringify(dbNotes), function(err,data){
         if(err) throw err;
     })
     console.log('delet route', dbNotes);
